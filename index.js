9515db36b8c6f61287ded201fc194639a05440ab
amamenko <amamenko@hotmail.com>
2021-03-07 00:19:22 -0500

    Added server build folder production script

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.post("/api/create", (req, res) => {
   const newAthlete = new Athlete({
     _id: new mongoose.Types.ObjectId(),
